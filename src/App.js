e632675a0bd4e1497c90c34d47b8c719dc1dbdbb
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2021-11-03 10:08:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>hello world by burhan</h1>
     </div>
   );
 }
